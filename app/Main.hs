04e47c29d8e20ef7e0042d8fc71815be8e4c6bc9
Yuya <tsukudani710@gmail.com>
2022-07-31 03:00:47 +0900

    add cui

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,51 @@
 module Main where
 
 import Lib
+import Lexer
+import Node 
+import Parser
+import Executer
+
+import qualified Data.Text as T
+
+quitCommand = [":q", ":quit"]
 
 main :: IO ()
 main = do
+    putStrLn "Hello"
+    putStrLn "this is micro Lisp interpreter"
+    loop 
+
+loop :: IO ()
+loop = do
     arg <- getLine
-    putStr arg
+    putStr ">> "
+    doArg arg 
+
+
+doArg :: String -> IO ()
+doArg arg | head arg == ':' = 
+    if arg `elem` quitCommand then do 
+        putStr "\n"
+        putStrLn "Bye"
+    else do 
+        putStrLn "invalid command"
+        loop
+doArg [] = loop
+doArg arg =   do 
+        let eTokens =  lexicalAnalyze (T.pack arg)
+        case eTokens of 
+            Left s -> do 
+                putStrLn (T.unpack s)
+                loop
+            Right tokens -> do 
+                let eSt = genST tokens
+                case eSt of 
+                  Left txt -> do 
+                    putStrLn (T.unpack txt)
+                    loop
+                  Right no -> do
+                    eval no
+                    loop 
+
+
